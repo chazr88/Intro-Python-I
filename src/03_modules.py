768128346184b98745457e9d508f2e1e16e1c8af
Chaz Rowe <chazr_88@hotmail.com>
2020-01-17 09:02:27 -0500

    finished the first 4

--- src/03_modules.py
+++ src/03_modules.py
@@ -16,16 +16,17 @@ import sys
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print(os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print(os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
